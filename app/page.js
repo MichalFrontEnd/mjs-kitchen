cbdc5917d1c18465e4a3c3b78b374eee456d52cb
Michal <michal.front@outlook.com>
2024-07-03 16:11:02 +0200

    Adds hompage styling

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import mainImg from '@/assets/mj.jpg'
+import mainImg from "@/assets/mj.jpg";
 
 import { Nav } from "@/components/molecules/nav";
 
@@ -8,16 +8,20 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-      <div className={styles.polaroid}>
-
-      <Image 
-        src={mainImg.src}
-        width={300}
-        height={300}
-        alt="m&j in Malaga"
-        className={styles.mainimg}/>
+        <div className={styles.polaroid}>
+          <Image
+            src={mainImg.src}
+            width={300}
+            height={300}
+            alt='m&j in Malaga'
+            className={styles.mainimg}
+          />
         </div>
-        </div>
+      <div className={styles.description}>
+        <h1>We are Michal & Joakim</h1>
+        <h2>And this is our recipe collection</h2>
+      </div>
+      </div>
       <Nav />
     </main>
   );
