65870f8053a9b5075e81ea442f2e9809f765fa74
Michal <michal.front@outlook.com>
2024-07-03 15:51:34 +0200

    Adds homepage image and animation

--- app/page.js
+++ app/page.js
@@ -1,16 +1,24 @@
-
 import styles from "./page.module.css";
-import Link from 'next/link';
+import Image from "next/image";
+import mainImg from '@/assets/mj.jpg'
 
-import { Nav } from '@/components/molecules/nav';
+import { Nav } from "@/components/molecules/nav";
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
+      <div className={styles.center}>
+      <div className={styles.polaroid}>
 
-        <Nav />
-
+      <Image 
+        src={mainImg.src}
+        width={300}
+        height={300}
+        alt="m&j in Malaga"
+        className={styles.mainimg}/>
+        </div>
+        </div>
+      <Nav />
     </main>
   );
 }
