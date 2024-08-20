c8b3f1ab496542cb6e85c90e880d9cc2441a9c0d
Michal <michal.front@outlook.com>
2024-08-20 18:18:46 +0200

    Redirection and small fixes

--- app/recipes/[recipeSlug]/page.jsx
+++ app/recipes/[recipeSlug]/page.jsx
@@ -1,37 +1,51 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import { getRecipe } from '@/lib/recipes';
-import { Instructions } from '@/components/molecules/instructions'
+import { getRecipe } from "@/lib/recipes";
+import { Instructions } from "@/components/molecules/instructions";
 import { notFound } from "next/navigation";
 
-export function RecipeDetailPage({params}) {
+export function RecipeDetailPage({ params }) {
   const recipe = getRecipe(params.recipeSlug);
+  console.log("recipe: ", recipe);
 
   if (!recipe) {
-    notFound()
+    notFound();
   }
-  
+
   return (
     <>
       <header className={styles.header}>
         <div className={styles.image}>
-          <Image src={recipe.image} alt={recipe.title} fill />
+          <Image
+            src={recipe.image}
+            alt={recipe.title}
+            fill
+          />
         </div>
         <div className={styles.headerText}>
           <h1>{recipe.title}</h1>
-          <p className={styles.creator}>
-            {/* conditional link */}
-            by {recipe.creator_link ? <a href={recipe.creator_link } className="highlight">{recipe.creator}</a> : <p className="highlight">{recipe.creator}</p>}
-          </p>
+          <div className={styles.creator}>
+            <p>by </p>
+            <p>
+              {recipe.creator_link ? (
+                <a
+                  href={recipe.creator_link}
+                  className='highlight'
+                >
+                  {recipe.creator}
+                </a>
+              ) : (
+                recipe.creator
+              )}
+            </p>
+          </div>
         </div>
       </header>
       <main className={styles.main}>
         <h2 className={styles.subheader}>Ingredients</h2>
-        {/* Ingredients component */}
         <p>Ingredients component Placeholder</p>
         <h2 className={styles.subheader}>Instructions</h2>
-        {/* Instructions component */}
-        <Instructions instructions={recipe.instructions}/>
+        <Instructions instructions={recipe.instructions} />
       </main>
     </>
   );
